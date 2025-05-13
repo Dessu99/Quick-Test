977c63988cf401bac38e8b70f6207a4c8486e689
Dessu99 <dexterjameskirkup@yahoo.co.uk>
2025-05-13 19:43:10 +0300

    Capsule Abstract

--- Assets/Scripts/CapsuleShape.cs
+++ Assets/Scripts/CapsuleShape.cs
@@ -5,20 +5,38 @@ public class CapsuleShape : ShapeBase
     public float radius = 0.5f;
     public float height = 2f;
 
-    public override void DrawShape()
-    {
-        float defaultHeight = 2f;
-        float defaultRadius = 0.5f;
+    public float pulseSpeed = 1f;
+    public float pulseAmount = 0.2f;
 
-        float scaleX = radius / defaultRadius;
-        float scaleZ = radius / defaultRadius;
-        float scaleY = height / defaultHeight;
-
-        transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
-    }
+    private float defaultRadius;
+    private float defaultHeight;
 
     void Start()
     {
+        defaultRadius = radius;
+        defaultHeight = height;
+    }
+
+    void Update()
+    {
+        // Animate radius and height using a sine wave
+        float pulse = Mathf.Sin(Time.time * pulseSpeed) * pulseAmount;
+
+        radius = defaultRadius + pulse;
+        height = defaultHeight + pulse;
+
         DrawShape();
+    }
+
+    public override void DrawShape()
+    {
+        float baseHeight = 2f;
+        float baseRadius = 0.5f;
+
+        float scaleX = radius / baseRadius;
+        float scaleZ = radius / baseRadius;
+        float scaleY = height / baseHeight;
+
+        transform.localScale = new Vector3(scaleX, scaleY, scaleZ);
     }
 }
